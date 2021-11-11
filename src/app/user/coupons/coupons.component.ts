2ca4fbb45f9174e1079b4a23e18e1eab2d4a9da4
DESKTOP-IJQFCV7\ASUS <sevgiyusifova98@gmail.com>
2021-11-11 23:21:06 +0400

    ccoupons page

--- src/app/user/coupons/coupons.component.ts
+++ src/app/user/coupons/coupons.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CouponsComponent implements OnInit {
 
+  public food_details: { restaurant: string, price: number}[] = [
+    {
+      restaurant: 'Dinero',
+      price: 56
+    },
+    {
+      restaurant: 'Metadore',
+      price: 73
+    },
+    {
+      restaurant: 'Zakura',
+      price: 73
+    },
+    {
+      restaurant: "BJ's Brewhouse",
+      price: 47
+    },
+    {
+      restaurant: 'Dinero',
+      price: 56
+    },
+    {
+      restaurant: 'Metadore',
+      price: 73
+    },
+    {
+      restaurant: 'Zakura',
+      price: 73
+    },
+    {
+      restaurant: "BJ's Brewhouse",
+      price: 47
+    },
+  ]
+
+
   constructor() { }
 
   ngOnInit() {}
